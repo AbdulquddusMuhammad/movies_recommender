a8cf93c44c717c90cc349d60b82606c5f4aad477
AbdulquddusMuhammad <abdulquddusmuhammad588@gmail.com>
2025-05-01 21:36:45 +0100

    Initial commit

---
+++ src/Components/Search.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Search = ({ searchTerm, setSearchTerm }) => {
+  return (
+    <div className="search">
+      <div>
+        <img src="../../public/search.svg" alt="" />
+        <input
+          type="text"
+          placeholder="Search through Thousands of Videos"
+          value={searchTerm}
+          onChange={(event) => {
+            setSearchTerm(event.target.value);
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Search;
